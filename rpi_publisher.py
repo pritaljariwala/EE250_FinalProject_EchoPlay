da92b19436b8d17de896fd127aa615b442ca8efb
Micr0waveMan <126802119+Micr0waveMan@users.noreply.github.com>
2025-12-07 23:38:20 +0000

    publisher push

--- rpi_publisher.py
+++ rpi_publisher.py
@@ -1,24 +1,28 @@
-# rpi_publisher.py
 import time
 import grovepi
 import paho.mqtt.client as mqtt
 
-SOUND_SENSOR = 0
-VOLUME_SENSOR = 2 
+SOUND_SENSOR = 14
 grovepi.pinMode(SOUND_SENSOR, "INPUT")
-grovepi.pinMode(VOLUME_SENSOR, "INPUT")
+rotary_sensor = 15
+grovepi.pinMode(rotary_sensor, "INPUT")
+BROKER_IP = "172.20.10.7"
 
-
-BROKER_IP = "172.20.10.6"   # replace with YOUR laptop's or Pi's broker
-
+value1 = grovepi.analogRead(SOUND_SENSOR)
 client = mqtt.Client()
 client.connect(BROKER_IP, 1883, 60)
-
+cnt = 0
 while True:
     try:
-        value = grovepi.analogRead(SOUND_SENSOR)
-        client.publish("project/raw_sound", value)
-        time.sleep(0.005)      # 200 Hz publishing rate
+        if cnt == 5:
+                value1 = grovepi.analogRead(SOUND_SENSOR)
+                cnt = 0
+
+        value2 = grovepi.analogRead(rotary_sensor)
+        payload = str(value1) + "," + str(value2)
+        client.publish("project/raw_sound", payload)
+        time.sleep(0.2)      # 5 Hz publishing rate
+        cnt = cnt +1
 
     except IOError:
         print("Sensor read error")
